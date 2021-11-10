660729b93ff4d7fba651d32e23b49916f7c2a344
Thyago de Oliveira Perez <thyagodoliveiraperez@gmail.com>
2021-11-10 16:11:38 -0300

    inicia ofertas

---
+++ src/main/java/com/femina/produto/View/OfertasView.java
@@ -0,0 +1,14 @@
+package main.java.com.femina.produto.View;
+
+public class OfertasView {
+
+    public void cadastrarOfertas(){
+        
+        int tamanhoListaProd = 1;//chamo a função que lista o produtos com desconto ex:listarProdDesc.size(); e atribuo o valor a variavel
+        //ex: List<ProdutoDesconto> produtoDescontoList = listarProdDesc.size();
+        for(int i = 0; i < tamanhoListaProd; i++){
+            //System.out.println(produtoDescontoList.get(i));
+        }
+    }
+
+}
